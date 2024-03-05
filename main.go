e8490799a66494ee166f9bfe53b23d443b5eec76
Thara Supasiti <sapiensis@gmail.com>
2024-03-05 23:08:15 +1100

    set up sigterm

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/websocket"
 )
@@ -15,15 +20,23 @@ var upgrader = websocket.Upgrader{
 func main() {
 	http.HandleFunc("/chat", handleWebSocket)
 
-	log.Printf("Starting a chat server at port: 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	server := &http.Server{Addr: ":8080"}
+	handleSigTerms(server)
+
+	slog.Info("Starting a chat server at port: 8080")
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		slog.Error("ListenAndServe() error", err)
+		os.Exit(1)
+	}
+
 }
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	// Upgrade HTTP connection to WebSocket
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("Error upgrading to WebSocket:", err)
+		slog.Error("Error upgrading to WebSocket:", err)
 		return
 	}
 	defer conn.Close()
@@ -32,17 +45,35 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		// Read message from the WebSocket
 		messageType, message, err := conn.ReadMessage()
 		if err != nil {
-			log.Println("Error reading message from WebSocket:", err)
+			slog.Error("Error reading message from WebSocket:", err)
 			break
 		}
 
 		// Print the received message
-		log.Printf("Received message: %s\n", message)
+		slog.Info(fmt.Sprintf("Received message: %s\n", message))
 
 		// Echo the message back to the client
 		if err := conn.WriteMessage(messageType, message); err != nil {
-			log.Println("Error echoing message:", err)
+			slog.Error("Error echoing message:", err)
 			break
 		}
 	}
+}
+
+// For gracefully shutdown server
+func handleSigTerms(server *http.Server) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-c
+		slog.Info("received SIGTERM, exiting...")
+
+		err := server.Shutdown(context.Background())
+		if err != nil {
+			slog.Error("server.Shutdown() error", err)
+		}
+
+		os.Exit(1)
+	}()
 }
